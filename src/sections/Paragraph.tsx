f3033f939325f19e6a45539a578c42754d950615
dulminawijegunarathne <dulminawijegunarathne@gmail.com>
2024-03-31 17:27:35 +0530

    fix: scroll fade in issue in paragraph

--- src/sections/Paragraph.tsx
+++ src/sections/Paragraph.tsx
@@ -1,28 +1,20 @@
-import React, {useState} from 'react';
 import {useInView} from 'react-intersection-observer';
+import React from 'react';
 
 type AnimatedLineProps = {
     text: string;
 };
 
 const AnimatedLine: React.FC<AnimatedLineProps> = ({text}) => {
-    const [hasBeenInView, setHasBeenInView] = useState(false);
     const {ref, inView} = useInView({
         threshold: 0.5,
-        // Removed `triggerOnce: true` to allow re-triggering inView status
+        triggerOnce: true,
     });
-
-    // Update the state when the element comes into view for the first time
-    if (inView && !hasBeenInView) {
-        setHasBeenInView(true);
-    }
 
     return (
         <p
             ref={ref}
-            className={`transition-opacity duration-1000 delay-200 transform text-center text-white text-[50px] 
-                        ${inView ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-5'} 
-                        ${hasBeenInView && !inView ? 'opacity-0 translate-y-5' : ''}`}
+            className={`transition-opacity duration-1000 ${inView ? 'opacity-100' : 'opacity-0'} transform translate-y-5 ${inView ? 'translate-y-0' : ''} text-center text-white text-[50px]`}
         >
             {text}
         </p>
@@ -33,7 +25,7 @@ export function Paragraph() {
     const lines = [
         "Metaroon Labs help to make your dream come true with",
         "software solutions. We are",
-        "skilled with multiple",
+        " skilled with multiple",
         "disciplines from software",
         "solutions, designer services",
         "to developing Web 3.0",
@@ -41,8 +33,8 @@ export function Paragraph() {
     ];
 
     return (
-        <div className="px-[100px]">
-            <div className="max-w-[1000px] mx-auto leading-[60px]">
+        <div className="px-[100px] my-72">
+            <div className="max-w-[1000px] mx-auto">
                 {lines.map((line, index) => (
                     <AnimatedLine key={index} text={line}/>
                 ))}
