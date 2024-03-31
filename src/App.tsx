3f2cdb0f1ecb7a804519fa20910829a5c765c358
dulminawijegunarathne <dulminawijegunarathne@gmail.com>
2024-03-31 11:30:34 +0530

    feat: add react router dom, create lab

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import './App.css'
 import {NavigationBar} from "@/components/NavigationBar.tsx";
-import {SectionTitle} from "@/components/SectionTitle.tsx";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Lab} from "@/pages/Lab.tsx";
+import {Home} from "@/pages/Home.tsx";
 
 function App() {
 
   return (
       <div className={"pt-[100px]"}>
           <NavigationBar/>
-          <SectionTitle primaryTitle={"Lorem Ipsum"} secondaryTitle={"Lorem ipsum lorem"} logoInclude={true}/>
+          <BrowserRouter>
+              <Routes>
+                  <Route path="/" element={<Home/>}/>
+                  <Route path="/lab" element={<Lab/>}/>
+              </Routes>
+          </BrowserRouter>
       </div>
   )
 }
---
+++ src/pages/Lab.tsx
@@ -0,0 +1,10 @@
+import {SectionTitle} from "@/components/SectionTitle.tsx";
+
+export function Lab() {
+
+    return (
+        <>
+            <SectionTitle primaryTitle={"Lorem Ipsum"} secondaryTitle={"Lorem ipsum lorem"} logoInclude={true}/>
+        </>
+    )
+}
