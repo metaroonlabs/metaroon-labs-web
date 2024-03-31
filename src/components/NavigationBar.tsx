a15170a804039b9f338fae15f2ad078e0f599552
dulminawijegunarathne <dulminawijegunarathne@gmail.com>
2024-03-31 15:21:47 +0530

    fix: navbar z index issue

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -8,7 +8,7 @@ export function NavigationBar() {
     return (
         <>
             <NavigationMenu
-                className={"min-w-full h-[80px] backdrop-blur bg-[#131313]/75 justify-between px-[100px] fixed top-0"}>
+                className={"min-w-full h-[80px] backdrop-blur bg-[#131313]/75 justify-between px-[100px] fixed top-0 z-50"}>
                 <Link>
                     <img src="/assets/images/logo/logo.png" alt="metaroon-logo" className={"w-[125px] cursor-pointer"}/>
                 </Link>
