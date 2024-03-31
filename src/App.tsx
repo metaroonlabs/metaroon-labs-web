57669c18bb32d3907e2a649d31c36cae0ffbaf82
dulminawijegunarathne <dulminawijegunarathne@gmail.com>
2024-03-31 11:22:01 +0530

    feat: create section title component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 import {NavigationBar} from "@/components/NavigationBar.tsx";
+import {SectionTitle} from "@/components/SectionTitle.tsx";
 
 function App() {
 
   return (
-      <>
+      <div className={"pt-[100px]"}>
           <NavigationBar/>
-      </>
+          <SectionTitle primaryTitle={"Lorem Ipsum"} secondaryTitle={"Lorem ipsum lorem"} logoInclude={true}/>
+      </div>
   )
 }
 
