22e0f02a091741dd0c8655100dce5d582357980c
Pasindu Lanka <106298729+apLanka@users.noreply.github.com>
2024-03-31 16:33:19 +0530

    feat: Add hero section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import {Services} from "@/sections/Services.tsx";
+import {Hero} from "@/sections/Hero.tsx";
 
 export function Home() {
 
     return (
         <>
             <div className={"text-white"}>
+                <Hero/>
                 <Services/>
             </div>
         </>
