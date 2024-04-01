1d3fe90605cf8e256f2d79ad5543b5613b53d53d
Pasindu Lanka <106298729+apLanka@users.noreply.github.com>
2024-04-01 21:19:39 +0530

    feat: Implement contact responsive

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,25 +1,28 @@
-import {Button} from "@/components/ui/button.tsx";
 import {ContactCard} from "@/components/ContactCard.tsx";
 
 export function Contact() {
     return (
-        <div>
-            <div className={'grid grid-rows-2 grid-flow-col gap-[20px] md:gap-[30px] lg:p-[100px] md:p-[25px] p-[25px]'}>
-                <div>
+        <div className={"lg:p-[100px] p-[25px]"}>
+            <div className={'flex flex-col md:flex-row items-center md:items-start justify-between gap-[50px] md:gap-0'}>
+                <div className={''}>
                     <div
-                        className={'text-[32px] lg:text-[50px] md:text-[40px] font-[700] leading-[50px] text-transparent bg-clip-text bg-gradient-to-b from-[#FFFFFF] from-[41.84%] to-[#131313] to-[99%]'}>Contact
+                        className={'flex-none text-[28px] lg:text-[50px] md:text-[40px] font-[700] leading-[50px] text-transparent bg-clip-text bg-gradient-to-b from-[#FFFFFF] from-[41.84%] to-[#131313] to-[99%]'}>Contact
                         US
                     </div>
                 </div>
-                <div className={"flex items-end"}>
-                    <Button variant="outline" className={"w-[78px] h-[159px] border-[2px] rounded-[110px]"}><img
-                        src="/assets/images/contact/up-arrow.png" alt="up-arrow" className={'h-24'}/></Button>
-                </div>
-                <div className={'row-span-2 grid grid-rows-2 grid-flow-col gap-[20px] md:gap-[30px] justify-end'}>
-                    <div><ContactCard whiteIconUrl={'/assets/images/contact/phone.png'} blackIconUrl={'/assets/images/contact/phone-black.png'} title={'Speak on'} secondTitle={'the Phone'}/></div>
-                    <div><ContactCard whiteIconUrl={'/assets/images/contact/mail-03.png'} blackIconUrl={'/assets/images/contact/mail-black.png'} title={'Send us'} secondTitle={'an email'}/></div>
-                    <div><ContactCard whiteIconUrl={'/assets/images/contact/linkedln.png'} blackIconUrl={'/assets/images/contact/linkedln-black.png'} title={'Follow us'} secondTitle={'on LinkedIn'}/></div>
-                    <div><ContactCard whiteIconUrl={'/assets/images/contact/telegram.png'} blackIconUrl={'/assets/images/contact/telegram-black.png'} title={'Join us'} secondTitle={'on Telegram'}/></div>
+                <div className={'grid grid-cols-2 grid-flow-row gap-[20px]'}>
+                    <ContactCard whiteIconUrl={'/assets/images/contact/phone.png'}
+                                      blackIconUrl={'/assets/images/contact/phone-black.png'} title={'Speak on'}
+                                      secondTitle={'the Phone'}/>
+                    <ContactCard whiteIconUrl={'/assets/images/contact/mail-03.png'}
+                                      blackIconUrl={'/assets/images/contact/mail-black.png'} title={'Send us'}
+                                      secondTitle={'an email'}/>
+                    <ContactCard whiteIconUrl={'/assets/images/contact/linkedln.png'}
+                                      blackIconUrl={'/assets/images/contact/linkedln-black.png'} title={'Follow us'}
+                                      secondTitle={'on LinkedIn'}/>
+                    <ContactCard whiteIconUrl={'/assets/images/contact/telegram.png'}
+                                      blackIconUrl={'/assets/images/contact/telegram-black.png'} title={'Join us'}
+                                      secondTitle={'on Telegram'}/>
                 </div>
             </div>
         </div>
