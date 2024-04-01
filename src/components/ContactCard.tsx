51c6960b53c28437f1b37900a3a21d099719ba9b
Pasindu Lanka <106298729+apLanka@users.noreply.github.com>
2024-04-01 13:11:55 +0530

    fix: Implement responsive for contact

--- src/components/ContactCard.tsx
+++ src/components/ContactCard.tsx
@@ -9,20 +9,20 @@ type ContactCardProps = {
 export function ContactCard(props: ContactCardProps) {
     return (
         <div
-            className={"group flex flex-col justify-between border-[1px] border-[#4A4A4A] rounded-[10px] w-[300px] h-[400px] p-[25px] hover:bg-[#FFFFFF] hover:text-black hover:cursor-pointer"}>
+            className={"group flex flex-col justify-between border-[1px] border-[#4A4A4A] rounded-[10px] lg:w-[300px] lg:h-[400px] md:w-[240px] md:h-[320px] sm:w-[210px] sm:h-[280px] w-[180px] h-[240px] p-[25px] hover:bg-[#FFFFFF] hover:text-black hover:cursor-pointer"}>
             <div className={"flex w-[70px] h-[70px] border-[1px] border-[#4A4A4A] rounded-full justify-center items-center group-hover:border-[#C7C7C7]"}>
                 <img className={"w-[35px] h-[35px] absolute group-hover:opacity-0 transition-opacity duration-500"} src={props.whiteIconUrl} alt="contact-img"/>
                 <img className={"w-[35px] h-[35px] absolute opacity-0 group-hover:opacity-100 transition-opacity duration-500"} src={props.blackIconUrl} alt="contact-img"/>
             </div>
             <div className={'flex items-center justify-between bottom-0'}>
-                <div className={'text-[22px] font-[700] leading-[30.8px]'}>{props.title} {props.secondTitle &&
+                <div className={'text-[14px] lg:text-[22px] md:text-[18px] font-[700] leading-[30.8px]'}>{props.title} {props.secondTitle &&
                     <>
                         <br/>
                         {props.secondTitle}
                     </>
                 }</div>
                 <div className={'flex-none'}>
-                    <div className={'flex justify-center items-center rounded-full w-[50px] h-[50px] bg-white group-hover:bg-black'}>
+                    <div className={'flex justify-center items-center rounded-full lg:w-[50px] lg:h-[50px] md:w-[45px] md:h-[45px] w-[32px] h-[32px] bg-white group-hover:bg-black'}>
                         <HiArrowNarrowRight className={'w-[20px] h-[20px] text-black group-hover:text-white'}/></div>
                 </div>
             </div>
--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -4,10 +4,10 @@ import {ContactCard} from "@/components/ContactCard.tsx";
 export function Contact() {
     return (
         <div>
-            <div className={'grid grid-rows-2 grid-flow-col p-[100px]'}>
+            <div className={'grid grid-rows-2 grid-flow-col gap-[20px] md:gap-[30px] lg:p-[100px] md:p-[25px] p-[25px]'}>
                 <div>
                     <div
-                        className={'text-[50px] font-[700] leading-[50px] text-transparent bg-clip-text bg-gradient-to-b from-[#FFFFFF] from-[41.84%] to-[#131313] to-[99%]'}>Contact
+                        className={'text-[32px] lg:text-[50px] md:text-[40px] font-[700] leading-[50px] text-transparent bg-clip-text bg-gradient-to-b from-[#FFFFFF] from-[41.84%] to-[#131313] to-[99%]'}>Contact
                         US
                     </div>
                 </div>
@@ -15,7 +15,7 @@ export function Contact() {
                     <Button variant="outline" className={"w-[78px] h-[159px] border-[2px] rounded-[110px]"}><img
                         src="/assets/images/contact/up-arrow.png" alt="up-arrow" className={'h-24'}/></Button>
                 </div>
-                <div className={'row-span-2 grid grid-rows-2 grid-flow-col gap-[30px] justify-end'}>
+                <div className={'row-span-2 grid grid-rows-2 grid-flow-col gap-[20px] md:gap-[30px] justify-end'}>
                     <div><ContactCard whiteIconUrl={'/assets/images/contact/phone.png'} blackIconUrl={'/assets/images/contact/phone-black.png'} title={'Speak on'} secondTitle={'the Phone'}/></div>
                     <div><ContactCard whiteIconUrl={'/assets/images/contact/mail-03.png'} blackIconUrl={'/assets/images/contact/mail-black.png'} title={'Send us'} secondTitle={'an email'}/></div>
                     <div><ContactCard whiteIconUrl={'/assets/images/contact/linkedln.png'} blackIconUrl={'/assets/images/contact/linkedln-black.png'} title={'Follow us'} secondTitle={'on LinkedIn'}/></div>
