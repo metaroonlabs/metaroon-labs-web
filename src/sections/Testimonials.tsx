4bd6f49ead899646c5b6f9c98f87ff6315e2915c
dulminawijegunarathne <dulminawijegunarathne@gmail.com>
2024-04-03 14:17:06 +0530

    chore: update testimonials

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -9,19 +9,19 @@ export function Testimonials() {
             <div className="flex flex-col xl:flex-row justify-center gap-5 px-[25px] lg:px-[100px]">
                 <div className="w-full xl:w-[52%] flex flex-col gap-5 justify-between">
                     <TestimonialCard
-                        testimonial={"Metaroon Labs has been a game-changer for my company. Their reasonable cost, frequent communication, and high efficiency has made them stand out from the competition, and it's been a pleasure to work with them!"}
+                        testimonial={"“Metaroon Labs has been a game-changer for my company. Their reasonable cost, frequent communication, and high efficiency has made them stand out from the competition, and it's been a pleasure to work with them!”"}
                         username={"Brian Ross"} designation={"CEO/Co Founder - Draft Labs"}
                         imgUrl={"/assets/images/testimonials/draft-labs.png"}/>
                     <TestimonialCard
-                        testimonial={"MetaRoon labs is a needle in a haystack, very often people find themselves disappointed with an outsourced dev team but this isn’t the case with MetaRoon. Their team is brilliant, kind and easy to work with."}
+                        testimonial={"“MetaRoon labs is a needle in a haystack, very often people find themselves disappointed with an outsourced dev team but this isn’t the case with MetaRoon. Their team is brilliant, kind and easy to work with.”"}
                         username={"Adin Zweigbaum"} designation={"Chief Innovation Officer - Wisdom Gaming"}
                         imgUrl={"/assets/images/testimonials/wisdom.png"}/>
                 </div>
                 <div className={"w-full xl:w-[48%]"}>
                     <TestimonialCard
-                        testimonial={"“Lorem ipsum dolor sit amet consectetur. Purus arcu curabitur rhoncus vitae felis metus in semper ut. Pretium maecenas nisl amet sapien mi. Eu dignissim fermentum urna iaculis dictum adipiscing in integer. Nisl ut a lectus commodo non viverra sodales nunc in. Ut ultricies sed proin in tellus ut aliquam cursus urna. Ut viverra sit iaculis dolor magna id sagittis sodales. Morbi nisl odio id eu felis nunc. Pellentesque ac integer ut venenatis. Sed lobortis enim ultrices ultricies ac nisl nullam commodo risus. Urna placerat egestas lectus eget tristique in tempor. Placerat felis sapien blandit vitae dui facilisi sed. Pulvinar amet purus lobortis rhoncus tempus id amet. Cursus risus ultrices orci sit. Volutpat aenean amet nisi dui nec pellentesque mauris habitant faucibus. Maecenas nisl tempor neque sed. Volutpat hendrerit egestas placerat.”"}
-                        username={"Vytas Butkevicius"} designation={"Founder at Outframe"}
-                        imgUrl={"/assets/images/testimonials/avatar.png"} largeCard={true}/>
+                        testimonial={"“My journey with Metaroon Labs has been a remarkable experience, characterized by effortless collaboration and exceptional communication. Throughout the duration of gaming projects, ranging from six to eight months, their approach to project management stood out as both efficient and effective. What I particularly appreciated was their adeptness in straddling the domains of Web 3.0 and game development, a testament to their broad competencies and innovative edge. The team's ability to navigate complex projects with ease, while maintaining an open and responsive line of communication, significantly contributed to the seamless progression and success of our endeavors. Engaging with Metaroon Labs not only elevated the quality of our projects but also enriched my professional growth and understanding of cutting-edge technologies in the digital landscape.”"}
+                        username={"Jared Belson"} designation={"Founder at EndFrame"}
+                        imgUrl={"/assets/images/testimonials/logo.jpg"} largeCard={true}/>
                 </div>
             </div>
         </>
