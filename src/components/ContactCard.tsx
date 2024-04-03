649009a4107d9eb0ff6ce7a8ccb764ffedb66f62
dulminawijegunarathne <dulminawijegunarathne@gmail.com>
2024-04-03 10:38:23 +0530

    chore: contact us card hover animation duration

--- src/components/ContactCard.tsx
+++ src/components/ContactCard.tsx
@@ -9,8 +9,9 @@ type ContactCardProps = {
 export function ContactCard(props: ContactCardProps) {
     return (
         <div
-            className={"group flex flex-col justify-between border-[1px] border-[#4A4A4A] rounded-[10px] lg:w-[300px] lg:h-[400px] md:w-[240px] md:h-[320px] sm:w-[210px] sm:h-[280px] w-[140px] h-[200px] p-[18px] sm:p-[25px] hover:bg-[#FFFFFF] hover:text-black hover:cursor-pointer"}>
-            <div className={"flex w-[40px] h-[40px] sm:w-[50px] sm:h-[50px] md:w-[70px] md:h-[70px] border-[1px] border-[#4A4A4A] rounded-full justify-center items-center group-hover:border-[#C7C7C7]"}>
+            className={"group flex flex-col justify-between border-[1px] border-[#4A4A4A] rounded-[10px] lg:w-[300px] lg:h-[400px] md:w-[240px] md:h-[320px] sm:w-[210px] sm:h-[280px] w-[140px] h-[200px] p-[18px] sm:p-[25px] hover:bg-[#FFFFFF] hover:text-black cursor-pointer duration-500"}>
+            <div
+                className={"flex w-[40px] h-[40px] sm:w-[50px] sm:h-[50px] md:w-[70px] md:h-[70px] border-[1px] border-[#4A4A4A] rounded-full justify-center items-center group-hover:border-[#C7C7C7] duration-500"}>
                 <img className={"w-[20px] h-[20px] sm:w-[25px] sm:h-[25px] md:w-[35px] md:h-[35px] absolute group-hover:opacity-0 transition-opacity duration-500"} src={props.whiteIconUrl} alt="contact-img"/>
                 <img className={"w-[20px] h-[20px] sm:w-[25px] sm:h-[25px] md:w-[35px] md:h-[35px] absolute opacity-0 group-hover:opacity-100 transition-opacity duration-500"} src={props.blackIconUrl} alt="contact-img"/>
             </div>
@@ -22,8 +23,10 @@ export function ContactCard(props: ContactCardProps) {
                     </>
                 }</div>
                 <div className={'flex-none'}>
-                    <div className={'flex justify-center items-center rounded-full lg:w-[50px] lg:h-[50px] md:w-[45px] md:h-[45px] sm:w-[32px] sm:h-[32px] w-[28px] h-[28px] bg-white group-hover:bg-black'}>
-                        <HiArrowNarrowRight className={'w-[20px] h-[20px] text-black group-hover:text-white'}/></div>
+                    <div
+                        className={'flex justify-center items-center rounded-full lg:w-[50px] lg:h-[50px] md:w-[45px] md:h-[45px] sm:w-[32px] sm:h-[32px] w-[28px] h-[28px] bg-white group-hover:bg-black duration-500'}>
+                        <HiArrowNarrowRight
+                            className={'w-[20px] h-[20px] text-black group-hover:text-white duration-500'}/></div>
                 </div>
             </div>
         </div>
