c589e2def0f6b92048a3f162fc0411b2c2589f03
dulminawijegunarathne <dulminawijegunarathne@gmail.com>
2024-04-04 11:02:25 +0530

    chore: update footer responsive layout

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -2,9 +2,10 @@ export function Footer() {
 
     return (
         <div className={"mx-[25px] lg:mx-[100px] border-t border-[#4A4A4A] text-center"}>
-            <div className={"flex flex-col gap-10 lg:flex-row justify-between items-center mt-8"}>
+            <div className={"flex flex-col gap-10 lg:flex-row justify-between lg:items-center mt-8"}>
                 <img src="/assets/images/logo/logo.png" alt="metaroon-logo" className={"w-[125px]"}/>
-                <div className={"flex flex-col items-center md:flex-row gap-5 md:gap-10"}>
+                <div
+                    className={"sm:flex text-start sm:text-center items-center flex-row sm:justify-between gap-5 md:gap-10"}>
                     <div>© 2024 metaroonlabs. All rights reserved.</div>
                     <a href="">Privacy-Policy</a>
                 </div>
