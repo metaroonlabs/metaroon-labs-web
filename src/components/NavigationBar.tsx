6501dff63e314ad54692f31491e6e4e251e3cad9
dulminawijegunarathne <dulminawijegunarathne@gmail.com>
2024-04-05 15:02:30 +0530

    chore: update navbar with dropdown menu for mobile

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -2,10 +2,21 @@ import {Link} from "@radix-ui/react-navigation-menu";
 import {NavigationMenu, NavigationMenuLink, navigationMenuTriggerStyle} from "@/components/ui/navigation-menu.tsx";
 import {Button} from "@/components/ui/button.tsx";
 import {FiChevronRight} from "react-icons/fi";
-import {Sheet, SheetContent, SheetTrigger} from "@/components/ui/sheet.tsx";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuRadioGroup,
+    DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu.tsx";
+import {useState} from "react";
 
 export function NavigationBar() {
     const calendlyUrl = 'https://calendly.com/bathiyaw';
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleMobileBtn = () => {
+        setIsOpen(!isOpen)
+    }
 
     return (
         <>
@@ -41,8 +52,44 @@ export function NavigationBar() {
                     <Button onClick={() => window.open(calendlyUrl, '_blank')} className={"hidden lg:flex"}>Book a
                         call <FiChevronRight
                         size={18}/></Button>
+
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild onClick={handleMobileBtn}>
+                            <Button className="lg:hidden w-[40px] h-[40px]" variant="outline" onClick={handleMobileBtn}>
+                                <img src="/assets/images/menu.png"
+                                     className={isOpen ? "min-w-[18px] rotate-s" : "min-w-[18px] rotate-a"}
+                                     alt="menu-icon"/>
+                            </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent className="w-56 mr-[25px] bg-white">
+                            <DropdownMenuRadioGroup value={"bottom"} className={"bg-white"}>
+                                <div className={" flex flex-col gap-2 justify-center text-[#131313]"}>
+                                    <Link href="#projects">
+                                        <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                                            Projects
+                                        </NavigationMenuLink>
+                                    </Link>
+                                    <Link href="#services">
+                                        <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                                            Services
+                                        </NavigationMenuLink>
+                                    </Link>
+                                    <Link href="#about">
+                                        <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                                            About
+                                        </NavigationMenuLink>
+                                    </Link>
+                                    <Link href="#contact">
+                                        <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                                            Contact
+                                        </NavigationMenuLink>
+                                    </Link>
+                                </div>
+                            </DropdownMenuRadioGroup>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
                 </div>
-                <Sheet>
+                {/*<Sheet>
                     <SheetTrigger asChild>
                         <Button className="lg:hidden w-[40px] h-[40px]" variant="outline">
                             <img src="/assets/images/menu.png" className={"min-w-[18px]"} alt="menu-icon"/>
@@ -71,7 +118,7 @@ export function NavigationBar() {
                                 size={18}/></Button>
                         </div>
                     </SheetContent>
-                </Sheet>
+                </Sheet>*/}
             </NavigationMenu>
         </>
     )
