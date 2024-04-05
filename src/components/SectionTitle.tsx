12a4734d8e81b7927f01bd09dc4b4689a82a29ab
dulminawijegunarathne <dulminawijegunarathne@gmail.com>
2024-04-05 15:17:58 +0530

    chore: make section title for three different sizes for mobile view

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -7,11 +7,18 @@ type SectionTitleProps = {
 }
 
 export function SectionTitle(props: SectionTitleProps) {
-    let textSizeClass = "text-[60px]"; // Default size
+    let textSizeClass = "text-[60px]";
     if (props.large) {
-        textSizeClass = "text-[51px]"; // Override if `large` prop is true
+        textSizeClass = "text-[51px]";
     } else if (props.medium) {
-        textSizeClass = "text-[56px]"; // Override if `medium` prop is true
+        textSizeClass = "text-[56px]";
+    }
+
+    let secondaryTitlePosition = 'top-0'
+    if (props.large) {
+        secondaryTitlePosition = "top-[-5px]"
+    } else if (props.medium) {
+        secondaryTitlePosition = 'top-[-2px]'
     }
 
     return (
@@ -20,7 +27,8 @@ export function SectionTitle(props: SectionTitleProps) {
                 className={`${textSizeClass} sm:text-[85px] md:text-[100px] lg:text-[115px] xl:text-[155px] 2xl:text-[170px] text-nowrap text-center font-semibold text-transparent bg-clip-text opacity-[15%] bg-gradient-to-b from-[#FFFFFF] from-20% to-[#131313] to-80%`}>
                 {props.primaryTitle}
             </div>
-            <div className={"absolute left-0 top-[-8px] sm:top-[-3px] md:top-0 xl:top-2 w-full flex justify-center"}>
+            <div
+                className={`absolute ${secondaryTitlePosition} left-0 sm:top-[-3px] md:top-0 xl:top-2 w-full flex justify-center`}>
                 <div
                     className={"text-[18px] sm:text-[30px] md:text-[35px] xl:text-[40px] 2xl:text-[45px] flex justify-center gap-4 lg:gap-8 items-center py-1 md:py-2 lg:py-3 px-5 md:px-10 xl:px-14 backdrop-blur font-bold rounded-[5px] md:rounded-[10px] border border-white text-transparent bg-clip-text bg-gradient-to-b from-[#FFFFFF] from-40% to-[#131313] to-90%"}>
                     {
