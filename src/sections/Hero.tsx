124dd582f804a383b5bd7531195fcb63dce050f4
Pasindu Lanka <106298729+apLanka@users.noreply.github.com>
2024-05-22 20:12:00 +0530

    feat: Add wave background

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,5 +1,6 @@
 import {FiChevronRight} from "react-icons/fi";
 import {Button} from "@/components/ui/button.tsx";
+import ThreeScene from "@/components/WaveBackground.tsx";
 
 export function Hero() {
     const calendlyUrl = 'https://calendly.com/bathiyaw';
@@ -7,7 +8,7 @@ export function Hero() {
     return (
         <div className={"relative min-h-[100vh] overflow-hidden"}>
             <img src="/assets/images/hero/bg-light.png" alt="bg-light"
-                 className={"absolute top-0 left-[-200px] md:left-[12%] max-w-[670px]"}/>
+                 className={"absolute top-0 left-[-200px] md:left-[12%] max-w-[670px] z-20"}/>
             <div className={'min-h-screen flex flex-col 2xl:max-w-[1420px] mx-auto'}>
                 <div className={"relative z-20 flex flex-col justify-center items-center flex-1 mt-[50px]"}>
                     <div className={"flex flex-col justify-end gap-[44px]"}>
@@ -37,7 +38,7 @@ export function Hero() {
                     </div>
                 </div>
                 <div
-                    className={"flex flex-col xl:flex-row gap-[24px] md:gap-[34px] justify-center items-center pb-[60px] text-[#BBBBBB] px-[25px] lg:px-[100px]"}>
+                    className={"flex flex-col xl:flex-row gap-[24px] md:gap-[34px] justify-center items-center pb-[60px] text-[#BBBBBB] px-[25px] lg:px-[100px] z-20"}>
                     Our clients:
                     <div className={"flex flex-wrap justify-center items-center gap-4 lg:gap-8 place-content-between"}>
                         <img src="/assets/images/hero/logo/draft-labs.png" alt="draft-labs"
@@ -57,10 +58,9 @@ export function Hero() {
                     </div>
                 </div>
             </div>
-            <img src="/assets/images/hero/Frame.png" alt="wave"
-                 className={'absolute w-full bottom-0 z-10 opacity-[30%]'}/>
+            <div className={'absolute w-full bottom-[-175px] z-10'}><ThreeScene/></div>
             <img src="/assets/images/hero/grain-texture.png" alt="grain"
-                 className={'absolute w-full h-full top-0 left-0 z-0 mix-blend-overlay'}/>
+                 className={'absolute w-full h-full top-0 left-0 z-15 mix-blend-overlay hidden'}/>
         </div>
     )
 }
