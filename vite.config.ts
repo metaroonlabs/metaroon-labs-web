b7c76c1450258baa1607ffbffe82bcae59be8c84
Dulmina Wijegunarathne <92733749+Dulmina98@users.noreply.github.com>
2024-09-11 23:11:10 +0530

    fix: routing issues from vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,9 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import {defineConfig} from "vite"
-import rewriteAll from 'vite-plugin-rewrite-all';
 
 export default defineConfig({
-  plugins: [react(), rewriteAll()],
-  server: {
-    host: true,
-    port: 3000,
-    open: true
-  },
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
